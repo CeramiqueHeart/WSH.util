59b4c6576a79a1d59fe43cc15bcdd8815869cc2d
CeramiqueHeart <mail@example.com>
2012-10-20 21:46:09 +0900

    first commit

---
+++ search_computer_account_creator.js
@@ -0,0 +1,60 @@
+var objRootDSE;
+var baseDN;
+var objConnection;
+var objCommand;
+
+try {
+  objRootDSE = GetObject("LDAP://rootDSE");
+  baseDN = objRootDSE.Get("defaultNamingContext");
+} catch(e) {
+  WScript.Echo("Can't connect LDAP server.");
+  WScript.Quit();
+}
+
+objConnection = new ActiveXObject("ADODB.Connection");
+objConnection.Provider = "ADsDSOObject";
+objConnection.Open     = "Active Directory Provider";
+
+objCommand = new ActiveXObject("ADODB.Command");
+objCommand.ActiveConnection = objConnection;
+
+if (WScript.Arguments.count() == 0) {
+  WScript.Echo("Usage: " + WScript.ScriptName + " <username1> [username2 ...]");
+  WScript.Quit();
+} else {
+  for (var i = 0; i < WScript.Arguments.count(); i++) {
+    search_computer_account(WScript.Arguments(i));
+    WScript.Echo();
+  }
+}
+
+
+function search_computer_account(username) {
+  var objWMI;
+  var result;
+  var objRecordSet;
+  
+  objWMI = GetObject("winmgmts:");
+  result = objWMI.ExecQuery("SELECT * FROM Win32_UserAccount WHERE LocalAccount = False AND Name = '" + username + "'");
+
+  if (result.count == 0) {
+    WScript.Echo (username + " is not found in directory.");
+    return;
+  } else {
+    var e = new Enumerator(result);
+    var sid = e.item().SID;
+  }
+
+  objCommand.CommandText = "<LDAP://cn=Computers," + baseDN + ">;(mS-DS-CreatorSID=" + sid + ");cn;Subtree";
+  objRecordSet = objCommand.Execute;
+
+  WScript.Echo(username + " (" + sid + ") joined " + objRecordSet.RecordCount + " computer(s).");
+
+  if (objRecordSet.RecordCount > 0) {
+    objRecordSet.MoveFirst;
+    while (! objRecordSet.EOF) {
+      WScript.Echo(objRecordSet.Fields("cn").Value);
+      objRecordSet.MoveNext;
+    }
+  }
+}
